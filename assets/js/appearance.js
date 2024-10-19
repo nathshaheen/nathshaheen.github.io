ebae37a94e0c77a2975da4fc69859190249e672c
Nathan Shaheen <nathshaheen1@gmail.com>
2024-10-19 21:39:40 +1100

    Added condensed navbar transitions

--- assets/js/appearance.js
+++ assets/js/appearance.js
@@ -13,3 +13,16 @@ toggleButton.addEventListener('click', function () {
         localStorage.setItem('theme', 'dark');
     }
 });
+
+function changeCss () {
+    var navElement = document.querySelector("nav");
+    this.scrollY > 100 ? navElement.style.paddingTop = "1.875rem" : navElement.style.paddingTop = "4.9375rem";
+    this.scrollY > 100 ? navElement.style.paddingBottom = "1rem" : navElement.style.paddingBottom = "2rem";
+
+    var navH1Element = document.querySelector("nav h1");
+    this.scrollY > 100 ? navH1Element.style.fontSize = "2rem" : navH1Element.style.fontSize = "4rem";
+
+    var navUlElement = document.querySelector("nav ul");
+    this.scrollY > 100 ? navUlElement.style.marginTop = "1rem" : navUlElement.style.marginTop = "2rem";
+}
+window.addEventListener("scroll", changeCss , false);
