79122e3c41113b2f8f570fca5e4c5fb93dc94490
Nathan Shaheen <nathshaheen1@gmail.com>
2024-10-25 20:42:32 +1100

    Changed navigation bar layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,14 +33,30 @@ lightModeToggle.addEventListener('click', () => {
 
 window.addEventListener("scroll", () => {
     const navBar = document.querySelector("nav");
-    this.scrollY > 100 ? navBar.style.paddingTop = "1.875rem" : navBar.style.paddingTop = "4.9375rem";
-    this.scrollY > 100 ? navBar.style.paddingBottom = "1rem" : navBar.style.paddingBottom = "2rem";
+    if (screen.width <= 480) {
+        this.scrollY > 100 ? navBar.style.paddingTop = "1.125rem" : navBar.style.paddingTop = "3.3541666667rem";
+    } else {
+        this.scrollY > 100 ? navBar.style.paddingTop = "1.375rem" : navBar.style.paddingTop = "3.71875rem";
+    }
+
+    this.scrollY > 100 ? navBar.style.paddingBottom = "2rem" : navBar.style.paddingBottom = "4rem";
 
     const navH1 = document.querySelector("nav h1");
     this.scrollY > 100 ? navH1.style.fontSize = "2rem" : navH1.style.fontSize = "4rem";
 
-    var navUl = document.querySelector("nav ul");
+    const navUl = document.querySelector("nav ul");
     this.scrollY > 100 ? navUl.style.marginTop = "1rem" : navUl.style.marginTop = "2rem";
+
+    const navLi = navUl.getElementsByTagName("li");
+    for (let i = 0; navLi[i]; i++) {
+        const temp = navLi[i].querySelector("a");
+
+        if (screen.width <= 480) {
+            this.scrollY > 100 ? temp.style.fontSize = "1.2rem" : temp.style.fontSize = "1.5rem";
+        } else {
+            this.scrollY > 100 ? temp.style.fontSize = "1rem" : temp.style.fontSize = "1.2rem";
+        }
+    }
 });
 
 /*
