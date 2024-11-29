b9fb243a8426bda56c24f4ae6cd261a8409caa21
Nathan Shaheen <nathshaheen1@gmail.com>
2024-11-30 10:17:58 +1100

    Removed page up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,20 +30,3 @@ const lightModeToggle = document.getElementById('appearance-toggle');
 lightModeToggle.addEventListener('click', () => {
     localStorage.getItem("lightAppearance") !== 'enabled' ? enableLightMode() : disableLightMode();
 });
-
-/*
- * Page up button
- */
-
-// const pageUpButton = document.getElementById("page-up");
-// window.onscroll = () => {
-//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//         pageUpButton.style.display = "block";
-//     } else {
-//         pageUpButton.style.display = "none";
-//     }
-// }
-//
-// const pageUp = () => {
-//     window.scrollTo({ top: 0, behavior: 'smooth' });
-// }
